ef51d37743da1d25d08fbc65c94201dace0bec70
Yiannis Kalogeris <yianniskalogeris@gmail.com>
2019-10-08 20:09:54 +0300

    Added code documentation and some changes in README.md

--- src/MGroup.MachineLearning/ILinearRegression.cs
+++ src/MGroup.MachineLearning/ILinearRegression.cs
@@ -7,21 +7,33 @@ using Tensorflow;
 namespace MGroup.MachineLearning
 {
 	/// <summary>
-	/// Interface of linear regression project
+	/// Interface for linear regression project.
 	/// </summary>
 	public interface ILinearRegression
 	{
 
 		/// <summary>
-		/// Obtain the optimal coefficients of the regression
+		/// Evaluates the optimal coefficients of the regression that minimize the least squares error
+		/// between the given values of the dependent variables and the ones predicted.
 		/// </summary>
+		/// <param name="X"> A <see cref="double"/> array containing the given values of the independent variables.</param>
+		/// <param name="Y"> A <see cref="double"/> array containing the corresponding values of the dependent variable.</param>
 		void Train(double[] X, double[] Y);
 
-		void Test(Session sess);
+		/// <summary>
+		/// Test the accuracy of the regression model on additional data.
+		/// </summary>
+		/// <param name="X"> A <see cref="double"/> array containing the additional values of the independent variables.</param>
+		/// <param name="Y"> A <see cref="double"/> array containing the corresponding values of the dependent variable.</param>
+		void Test(double[] X, double[] Y);
 
 		/// <summary>
-		/// Use the computed regression coefficients to make predictions for new data
+		/// Utilizes the regression model to make predictions for new data.
 		/// </summary>
+		/// <param name="X">A <see cref="double"/> array containing new values of the independent variables,
+		/// whose outcome we want to predict.</param>
+		/// <returns> A <see cref="float"/> array containing the predicted values of the dependent variables, a <see cref="float"/>
+		/// containing the optimal weight and a <see cref="float"/> containing the optimal bias for the model.</returns>
 		(float[],float,float) Predict(double[] X);
 
 	}
--- src/MGroup.MachineLearning/INeuralNetwork.cs
+++ src/MGroup.MachineLearning/INeuralNetwork.cs
@@ -6,23 +6,31 @@ using Tensorflow;
 
 namespace MGroup.MachineLearning
 {
+	/// <summary>
+	/// Interface for Neural Network project.
+	/// </summary>
 	public interface INeuralNetwork
 	{
 		/// <summary>
-		/// Set true to import the computation graph instead of building it.
+		/// Evaluates the weight coefficients and biases of the neural network that minimize the mean-square error
+		/// between the given values of the dependent variables and the ones predicted.
 		/// </summary>
-		bool IsImportingGraph { get; set; }
-
-		/// <summary>
-		/// Build dataflow graph and train a neural network
-		/// </summary>
+		/// <param name="X">A <see cref="double"/> array containing the given values of the independent variables.</param>
+		/// <param name="Y">A <see cref="double"/> array containing the corresponding values of the dependent variable.</param>
 		void Train(double[,] X, float[] Y);
 
-		void Test(Session sess);
+		/// <summary>
+		/// Tests the accuracy of the neural network on additional data.
+		/// </summary>
+		/// <param name="X">A <see cref="double"/> array containing the additional values of the independent variables.</param>
+		/// <param name="Y">A <see cref="double"/> array containing the corresponding values of the dependent variable.</param>
+		void Test(double[,] X, double[] Y);
 
 		/// <summary>
-		/// Use the neural network to make predictions for new data
+		/// Utilizes the neural network to make predictions for new data.
 		/// </summary>
+		/// <param name="X">A <see cref="double"/> array containing the new values of the independent variables, whose outcome we want to predict.</param>
+		/// <returns> A <see cref="float"/> array containing the predicted values of the dependent variables.</returns>
 		float[] Predict(double[,] X);
 	}
 }
