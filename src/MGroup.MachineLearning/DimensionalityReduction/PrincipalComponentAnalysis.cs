6e407dadbc1b7843dac2d31bda21088968a9ab96
Yiannis Kalogeris <yianniskalogeris@gmail.com>
2019-11-09 16:00:57 +0200

    Started adding Dimensionality Reduction classes

---
+++ src/MGroup.MachineLearning/DimensionalityReduction/PrincipalComponentAnalysis.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MGroup.LinearAlgebra;
+
+namespace MGroup.MachineLearning.DimensionalityReduction
+{
+	class PrincipalComponetAnalysis
+	{
+		public double[,] PCA(double[,] rawData)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
