058b31c3a951f72356159554238dd1ea9c6742af
Yiannis Kalogeris <yianniskalogeris@gmail.com>
2019-11-09 18:18:31 +0200

    some fixes in DiffusionMaps

--- src/MGroup.MachineLearning/DimensionalityReduction/DiffusionMaps.cs
+++ src/MGroup.MachineLearning/DimensionalityReduction/DiffusionMaps.cs
@@ -14,6 +14,7 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 		private int differentialOperator = 1;  // 1:Laplace-Beltrami, 2:generator of grad system
 		private int numberOfEigenvectors = 3;
 		private double[,] dataSet = DiffusionMapsAlgorithm.ImportData();
+		double alpha, beta;
 
 		// read the matrix containing the problem data
 		private static double[,] ImportData()
@@ -55,11 +56,10 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 			}
 			double[] rho0 = evaluateRho0(sortedDistances, NNofKDE);
 			// pre-kernel used with ad hoc bandwidth only for estimating dimension and sampling density
-			double[,] dt = evaluateDt(rho0, indices, numberOfKNN);
+			double[,] dt = evaluateDt(sortedDistances, rho0, indices, numberOfKNN);
 			// tune epsilon on the pre-kernel
 			double[] dpreGlobal = evaluateDpreGlobal(epss, dt, numberOfKNN, N);
-			double maxval = findMaxvalMaxind().Item1;
-			double maxind = findMaxvalMaxind().Item2;
+			(double maxval, int maxind) = findMaxvalMaxind(dpreGlobal, epss);
 			double dim = 2 * maxval;
 
 			// use ad hoc bandwidth function, rho0, to estimate the density
@@ -67,7 +67,7 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 			{
 				for (var j = 0; j < dt.GetLength(1); j++)
 				{
-					dt[i, j] = Math.Exp(-dt[i, j] / (2 * epss[maxind])) / Math.pow(2 * Math.PI * epss[maxind], (dim / 2));
+					dt[i, j] = Math.Exp(-dt[i, j] / (2 * epss[maxind])) / Math.Pow(2 * Math.PI * epss[maxind], (dim / 2));
 				}
 			}
 
@@ -96,37 +96,37 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 
 			if (differentialOperator == 1)
 			{
-				double beta = -1 / 2;
-				double alpha = -dim / 4 + 1 / 2;
+				beta = -1 / 2;
+				alpha = (-dim / 4) + (1 / 2);
 			}
 			else if (differentialOperator == 2)
 			{
-				double beta = -1 / 2;
-				double alpha = -dim / 4;
+				beta = -1 / 2;
+				alpha = -dim / 4;
 			}
 
-			double c1 = 2 - 2 * alpha + dim * beta + 2 * beta;
-			double c2 = 0.5 - 2 * alpha + 2 * dim * alpha + dim * beta / 2 + beta;
+			double c1 = 2 - (2 * alpha) + (dim * beta) + (2 * beta);
+			double c2 = 0.5 - (2 * alpha) + (2 * dim * alpha) + (dim * beta / 2) + beta;
 
 			for (var i = 0; i < sortedDistances.GetLength(0); i++)
 			{
-				for (var j = 0; j < sortedDistances.GetLength(1); j++)
+				for (int j = 0; j < sortedDistances.GetLength(1); j++)
 				{
-					sortedDistances[i, j] = Math.Exp(sortedDistances[i, j], 2);
+					sortedDistances[i, j] = Math.Pow(sortedDistances[i, j], 2);
 				}
 			}
 
 			// construct bandwidth function rho(x) from the sampling density estimate
-			double[] rho = new double[qest.GetLength()];
+			double[] rho = new double[qest.Length];
 			double sumRho = 0;
-			for (var i = 1; i < qest.GetLength(); i++)
+			for (var i = 1; i < qest.Length; i++)
 			{
 				rho[i] = Math.Pow(qest[i], beta);
 				sumRho += rho[i];
 			}
-			for (var i = 1; i < qest.GetLength(); i++)
+			for (var i = 1; i < qest.Length; i++)
 			{
-				rho[i] = rho[i] / sumRho * rho.GetLength();
+				rho[i] = rho[i] / sumRho * rho.Length;
 			}
 
 			// construct the exponent of K^S_epsilon
@@ -187,11 +187,10 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 
 		}
 
-		private static Tuple<int[,], double[,]> KnnSearch(double[,] trainSamples, double[,] testSamples, int K)
+		private static (int[,], double[,]) KnnSearch(double[,] trainSamples, double[,] testSamples, int K)
 		{
 
 			var indices = new int[testSamples.GetLength(0), testSamples.GetLength(0)];
-
 			var testNumber = testSamples.GetLength(0);
 			var trainNumber = trainSamples.GetLength(0);
 			// Declaring these here so that I don't have to 'new' them over and over again in the main loop, 
@@ -208,18 +207,20 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 				// For every test sample, calculate distance from every training sample
 				for (var trn = 0; trn < trainNumber; trn++)
 				{
-					var dist = GetDistance(testSamples[tst], trainSamples[trn]);
+					double[] testSample= Enumerable.Range(0, testSamples.GetLength(1)).Select(x => testSamples[tst, x]).ToArray();
+					double[] trainSample = Enumerable.Range(0, trainSamples.GetLength(1)).Select(x => trainSamples[tst, x]).ToArray();
+
+					double dist = GetDistance(testSample, trainSample);
 					// Storing distance as well as index 
-					distances[trn, 0] = dist;
-					distances[trn, 1] = trn;
+					distances[trn][0] = dist;
+					distances[trn][1] = trn;
 				};
 
 				// Sort distances and take top K (?What happens in case of multiple points at the same distance?)
 				var votingDistances = distances.AsParallel().OrderBy(t => t[0]).Take(K);
-
 			}
 
-			return new Tuple<int[,], double[,]>(indices, sortedDistances);
+			return (indices, sortedDistances);
 		}
 
 		private static double GetDistance(double[] sample1, double[] sample2)
@@ -227,17 +228,18 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 			var distance = 0.0;
 			// assume sample1 and sample2 are valid i.e. same length 
 
-			for (var i = 0; i < sample1.GetLength; i++)
+			for (var i = 0; i < sample1.Length; i++)
 			{
 				var temp = sample1[i] - sample2[i];
 				distance += temp * temp;
 			}
+			distance = Math.Sqrt(distance);
 			return distance;
 		}
 
 		private static double[] evaluateRho0(double[,] dist, int k)
 		{
-			distNew = new double[dist.GetLength(0), k - 1];
+			double[,] distNew = new double[dist.GetLength(0), k - 1];
 			for (var i = 0; i < distNew.GetLength(0); i++)
 			{
 				for (var j = 0; j < k - 1; j++)
@@ -246,7 +248,7 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 				}
 			}
 
-			rho0 = new double[dist.GetLength(0)];
+			double[] rho0 = new double[dist.GetLength(0)];
 			for (var i = 0; i < distNew.GetLength(0); i++)
 			{
 				for (var j = 0; j < distNew.GetLength(1); j++)
@@ -260,8 +262,8 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 
 		private static double[,] evaluateDt(double[,] dist, double[] rho0, int[,] indices, int k)
 		{
-			temp = new double[dist.GetLength(0), k];
-			dt = new double[dist.GetLength(0), k];
+			double[,] temp = new double[dist.GetLength(0), k];
+			double[,] dt = new double[dist.GetLength(0), k];
 			for (var i = 0; i < dist.GetLength(0); i++)
 			{
 				for (var j = 0; j < k; j++)
@@ -283,9 +285,9 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 
 		private static double[] evaluateDpreGlobal(double[] epss, double[,] dt, int k, int N)
 		{
-			double[,] temp = new double[dt.GetLength(0), dt.GetLenth(1)];
-			double[] dpreGlobal = new double[epss.GetLength];
-			for (var i = 0; i < epss.GetLength(); i++)
+			double[,] temp = new double[dt.GetLength(0), dt.GetLength(1)];
+			double[] dpreGlobal = new double[epss.Length];
+			for (var i = 0; i < epss.Length; i++)
 			{
 				double tempSum = 0;
 				for (var j = 0; j < dt.GetLength(0); j++)
@@ -301,21 +303,21 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 			return dpreGlobal;
 		}
 
-		private static Tuple<double, int> findMaxvalMaxind(double[] dpreGlobal, double[] epss)
+		private static (double, int) findMaxvalMaxind(double[] dpreGlobal, double[] epss)
 		{
-			double[] temp1 = new double[dpreGlobal.GetLength() - 1];
-			double[] temp2 = new double[epss.GetLength() - 1];
-			double[] temp3 = new double[epss.GetLength() - 1];
-			for (var i = 0; i < dpreGlobal.GetLength() - 1; i++)
+			double[] temp1 = new double[dpreGlobal.Length - 1];
+			double[] temp2 = new double[epss.Length - 1];
+			double[] temp3 = new double[epss.Length - 1];
+			for (var i = 0; i < dpreGlobal.Length - 1; i++)
 			{
-				temp1[i] = Math.Log(dpreGlobal(i + 1)) - Math.Log(dpreGlobal(i));
-				temp2[i] = Math.Log(epss(i + 1)) - Math.Log(epss(i));
+				temp1[i] = Math.Log(dpreGlobal[i + 1]) - Math.Log(dpreGlobal[i]);
+				temp2[i] = Math.Log(epss[i + 1]) - Math.Log(epss[i]);
 				temp3[i] = temp1[i] / temp2[i];
 			}
 			double maxval = temp3.Max();
 			int maxind = temp3.ToList().IndexOf(maxval);
 
-			return new Tuple<double, int>(maxval, maxind);
+			return (maxval, maxind);
 		}
 
 		private static double[,] reshapeDt(double[,] dt, int[,] indices, int N, int k)
@@ -339,15 +341,15 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 				}
 			}
 
-			int[] temp4 = Enumerable.Repeat(temp3, k).ToArray();
+			int[][] temp4 = Enumerable.Repeat(temp3, k).ToArray();
 
 			double[,] reshapedDt = new double[N, N];
 			count = 0;
-			for (var i = 0; i < N; i++)
+			for (int i = 0; i < N; i++)
 			{
-				for (var j = 0; j < N; j++)
+				for (int j = 0; j < N; j++)
 				{
-					reshapedDt[j, i] = dt[temp2[count], temp1[count]];
+					reshapedDt[j, i] = dt[temp2[count], temp3[count]];
 					count += 1;
 				}
 			}
@@ -356,8 +358,8 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 
 		private static double evaluateFinalEpsilon(double[] epss, double[,] sortedDistances, int N, int numberOfKNN)
 		{
-			double[] s = new double[epss.GetLength()];
-			for (var i = 0; i < epss.GetLength(); i++)
+			double[] s = new double[epss.Length];
+			for (var i = 0; i < epss.Length; i++)
 			{
 				double[,] temp = new double[sortedDistances.GetLength(0), sortedDistances.GetLength(1)];
 				double tempSum = 0;
@@ -365,17 +367,17 @@ namespace MGroup.MachineLearning.DimensionalityReduction
 				{
 					for (var q = 0; q < sortedDistances.GetLength(1); q++)
 					{
-						temp[j, q] = Math.Exp[sortedDistances[j, q] / (4 * epss[i])];
+						temp[j, q] = Math.Exp(sortedDistances[j, q] / (4 * epss[i]));
 						tempSum += temp[j, q];
 					}
 				}
 				s[i] = tempSum / (N * numberOfKNN);
 			}
 
-			double[] temp2 = new double[epss.GetLength()];
-			for (var i = 0; i < epss.GetLength() - 1; i++)
+			double[] temp2 = new double[epss.Length];
+			for (var i = 0; i < epss.Length - 1; i++)
 			{
-				temp2[i] = (Math.Log(s[i + 1]) - Math.Log(s[i]) / (Math.Log(epss[i + 1]) - Math.Log(epss[i]);
+				temp2[i] = (Math.Log(s[i + 1]) - Math.Log(s[i])) / (Math.Log(epss[i + 1]) - Math.Log(epss[i]));
 			}
 
 			double maxval = temp2.Max();
